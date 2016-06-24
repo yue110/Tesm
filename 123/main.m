5d3a747295aa1a202ba0249e0164ff9e3a87944d
Cynthia-yue <student@xiaoyao.local>
2016-06-24 16:52:32 +0800

    yue
    add

---
+++ 123/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  123
+//
+//  Created by student on 16/6/24.
+//  Copyright © 2016年 小妖. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
